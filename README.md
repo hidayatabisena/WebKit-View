e0b977cfe7b48d39077216e30a4890f92a385e2b
Hidayat Abisena <plegmasena@gmail.com>
2023-02-22 10:41:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WebKit View with Navigation Controller UIKit
+Just for educational purposes
+
+![Screenshot](https://res.cloudinary.com/moyadev/image/upload/v1677037238/sena/Xcode_22-02-2023_at_10.33_2x_tyqrzj.png)
+
---
+++ WebView/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by Hidayat Abisena on 22/02/23.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        if let url = URL(string: "https://www.livecoinwatch.com/price/Bitcoin-BTC?tab=1") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        
+        navigationController?.navigationBar.barTintColor = UIColor(red: 0.0, green: 0.5, blue: 1.0, alpha: 1.0)
+        
+        let backButton = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        let forwardButton = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
+        let reloadButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        navigationItem.leftBarButtonItems = [backButton, forwardButton]
+        navigationItem.rightBarButtonItems = [reloadButton]
+        
+    }
+
+
+}
+
